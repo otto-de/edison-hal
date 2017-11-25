2c2a585cbab6350b2ff25a66db21f6c94af67bf8
Guido Steinacker <guido.steinacker@otto.de>
2017-11-25 14:55:30 +0100

    Fixed some CuriTemplate issues Improved test coverage Added JavaDoc

--- src/main/java/de/otto/edison/hal/CuriTemplate.java
+++ src/main/java/de/otto/edison/hal/CuriTemplate.java
@@ -5,26 +5,65 @@ import java.util.Optional;
 
 /**
  * <p>
- *     A utility class used to handle custom link-relation type URIs and CURI templates.
+ *     A utility class used to handle user-defined link-relation types and CURI templates.
  * </p>
  * <p>
- * Example usage:
+ *     A CURI is a {@link Link#isTemplated() templated Link} with {@link Link#getRel() link-relation type }
+ *     {@code curies}, a {@link Link#name name} and a {@link Link#getHref() href} containing a single
+ *     placeholder {@code {rel}}:
  * </p>
  * <pre><code>
- *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
- *     String rel = "http://example.org/rels/product";
- *     String curiedRel = "x:product";
- *
- *     curiTemplateFor(curi).matches(rel) =&gt; true
- *
- *     curiTemplateFor(curi).curiedRelFrom(rel) =&gt; "x:product"
- *
- *     curiTemplateFor(curi).relPlaceHolderFrom(rel) =&gt; "product"
- *
- *     curiTemplateFor(curi).expand(rel) =&gt; "http://example.org/rels/product"
- *     curiTemplateFor(curi).expand(curiedRel) =&gt; "http://example.org/rels/product"
- *
+ *     {
+ *         "_links": {
+ *             "curies: [
+ *                  {"name": "x", "href": "http://example.com/rels/{rel}", "templated": true}
+ *             ]
+ *         }
+ *     }
  * </code></pre>
+ * <p>
+ *     or:
+ * </p>
+ * <pre><code>
+ *     Link curi = Link.curi("x", "http://example.com/rels/{rel}");
+ * </code></pre>
+ * <p>
+ *     You can construct a {@code CuriTemplate} from a curi like this:
+ * </p>
+ * <pre><code>
+ *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+ * </code></pre>
+ * <p>
+ *     A Link-relation type (rel) specifies the semantics of a {@link Link}. A {@code next} link, for example, points to
+ *     the next resource (page) in a sequence of paged resources. While rels like {@code self}, {@code next} or
+ *     {@code prev} are predefined types, you may define user-defined link-relation types as well. If you need to use a
+ *     user-defined rel like, for example a rel used to refer to a user resource, you should use URIs like
+ *     {@code http://example.org/rels/product}.
+ * </p>
+ * <p>
+ *     Because user-defined link-relation types (or custom rels) are quite verbose, CURIES can be used to abbreviate
+ *     them. The following example shows how this will look like in HAL+JSON:
+ * </p>
+ * <pre><code>
+ *     {
+ *         "_links": {
+ *             "curies: [
+ *                  {"name": "x", "href": "http://example.com/rels/{rel}", "templated": true}
+ *             ],
+ *             "x:product": [
+ *                  {"href": "http://example.com/products/42"},
+ *                  {"href": "http://example.com/products/43"},
+ *                  {"href": "http://example.com/products/44"},
+ *                  {"href": "http://example.com/products/45"}
+ *             ],
+ *             "x:shoppingCart": {"href": "http://example.com/shoppingCart/4711"}
+ *         }
+ *     }
+ * </code></pre>
+ * <p>
+ *     Sometimes it is necessary to construct CURIED rels from a expanded rel, or vice versa. The {@code CuriTemplate}
+ *     can be used to do things like this.
+ * </p>
  * @since 2.0.0
  */
 public class CuriTemplate {
@@ -32,11 +71,12 @@ public class CuriTemplate {
     private static final String REL_PLACEHOLDER = "{rel}";
 
     private final String relPrefix;
+    private final String curiedRelPrefix;
     private final String relSuffix;
     private final Link curi;
 
     private CuriTemplate(final Link curi) {
-        if (!curi.getRel().equals("curies")) {
+        if (!curi.getRel().equals("curies") || curi.getName().length() == 0) {
             throw new IllegalArgumentException("Parameter is not a CURI link.");
         }
         if (!curi.getHref().contains(REL_PLACEHOLDER)) {
@@ -44,63 +84,254 @@ public class CuriTemplate {
         }
         final String curiHref = curi.getHref();
         relPrefix = curi.getHref().substring(0, curiHref.indexOf(REL_PLACEHOLDER));
+        curiedRelPrefix = curi.getName() + ":";
         relSuffix = curi.getHref().substring(curiHref.indexOf(REL_PLACEHOLDER) + REL_PLACEHOLDER.length(), curiHref.length());
         this.curi = curi;
     }
 
+    /**
+     * Creates a CuriTemplate from a CURI Link.
+     *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     * </code></pre>
+     *
+     * @param curi a CURI Link
+     * @return CuriTemplate
+     * @throws IllegalArgumentException if the parameter is not a valid CURI.
+     * @since 2.0.0
+     */
     public static CuriTemplate curiTemplateFor(final Link curi) {
         return new CuriTemplate(curi);
     }
 
     /**
-     * Returns a CuriTemplate that is {@link #matches matching} the rel parameter, or empty if no matching CURI is found.
+     * Returns an optional CuriTemplate that is {@link #isMatching matching} the rel parameter, or empty if no matching CURI is found.
      *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     List&lt;Link&gt; curies = asList(
+     *          Link.curi("x", "http://example.org/rels/{rel}"),
+     *          Link.curi("y", "http://example.com/link-relations/{rel}")
+     *     );
+     *     String rel = "y:example";
+     *     CuriTemplate template = CuriTemplate.matchingCuriTemplateFor(curies, rel);
+     * </code></pre>
+     * <p>
+     *     The returned UriTemplate is created from the second CURI, as only this is matching the curied rel parameter
+     *     {@code y:example}. Because of this, {@link #expandedRelFrom(String) template.expandedRelFrom(rel)} will
+     *     return {@code http://example.com/link-relations/example}
+     * </p>
      * @param curies a List of curies Links
      * @param rel the link-relation type to check against the curies
      * @return optional CuriTemplate
+     * @throws IllegalArgumentException if the {@code curies} parameter contains non-CURI links.
+     * @since 2.0.0
      */
     public static Optional<CuriTemplate> matchingCuriTemplateFor(final List<Link> curies, final String rel) {
         return curies
                 .stream()
                 .map(CuriTemplate::curiTemplateFor)
-                .filter(t->t.matches(rel))
+                .filter(t->t.isMatching(rel))
                 .findAny();
     }
 
     /**
      * Returns true, if the given link-relation type is matching the CuriTemplate pattern, false if not.
      *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     *
+     *     // Returns true:
+     *     template.isMatching("http://example.org/rels/product");
+     *     template.isMatching("x:product"):
+     *
+     *     // Returns false:
+     *     template.isMatching("http://example.com/link-relations/product");
+     *     template.isMatching("y:product");
+     * </code></pre>
+     *
      * @param rel a link-relation type.
      * @return boolean
+     * @since 2.0.0
      */
-    public boolean matches(final String rel) {
-        return rel.startsWith(relPrefix) && rel.endsWith(relSuffix);
-    }
-
-    public String relPlaceHolderFrom(final String rel) {
-        if (matches(rel)) {
-            return rel.substring(relPrefix.length(), rel.length() - relSuffix.length());
-        } else {
-            throw new IllegalArgumentException("Rel is not matching the CURI template.");
-        }
-
-    }
-
-    public String curiedRelFrom(final String rel) {
-        if (matches(rel)) {
-            return curi.getName() + ":" + relPlaceHolderFrom(rel);
-        } else {
-            throw new IllegalArgumentException("Rel is not matching the CURI template.");
-        }
+    public boolean isMatching(final String rel) {
+        return isMatchingExpandedRel(rel) || isMatchingCuriedRel(rel);
     }
 
     /**
-     * Returns an expanded / full URI for a link-relation type.
+     * Returns true, if the given link-relation type is a CURI rel matching the CuriTemplate pattern, false if not.
      *
-     * @param rel the - possibly curied - link-relation type to expand.
-     * @return URL
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     *
+     *     // Returns true:
+     *     template.isMatchingCuriedRel("x:product"):
+     *
+     *     // Returns false:
+     *     template.isMatchingCuriedRel("http://example.org/rels/product");
+     *     template.isMatchingCuriedRel("http://example.com/link-relations/product");
+     *     template.isMatchingCuriedRel("y:product");
+     * </code></pre>
+     *
+     * @param rel a link-relation type.
+     * @return boolean
+     * @since 2.0.0
      */
-    public String expand(final String rel) {
-         return curi.getHrefAsTemplate().set("rel", relPlaceHolderFrom(rel)).expand();
+    public boolean isMatchingCuriedRel(final String rel) {
+        return rel.startsWith(curiedRelPrefix);
+    }
+
+    /**
+     * Returns true, if the given link-relation type is a non-CURI rel matching the CuriTemplate pattern, false if not.
+     *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     *
+     *     // Returns true:
+     *     template.isMatchingExpandedRel("http://example.org/rels/product");
+     *
+     *     // Returns false:
+     *     template.isMatchingExpandedRel("x:product"):
+     *     template.isMatchingExpandedRel("http://example.com/link-relations/product");
+     *     template.isMatchingExpandedRel("y:product");
+     * </code></pre>
+     *
+     * @param rel a link-relation type.
+     * @return boolean
+     * @since 2.0.0
+     */
+    public boolean isMatchingExpandedRel(final String rel) {
+        return rel.startsWith(relPrefix) && rel.endsWith(relSuffix);
+    }
+
+    /**
+     * Returns the placeholder from a link-relation type that is matching the {@code CuriTemplate}.
+     *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     *
+     *     // Returns "product":
+     *     template.relPlaceHolderFrom("http://example.org/rels/product");
+     *     template.relPlaceHolderFrom("x:product"):
+     *
+     *     // Throws IllegalArgumentException:
+     *     template.relPlaceHolderFrom("http://example.com/link-relations/product");
+     *     template.relPlaceHolderFrom("y:product");
+     * </code></pre>
+     *
+     * @param rel a link-relation type.
+     * @return boolean
+     * @throws IllegalArgumentException if the {@code rel} parameter does not match the {@code CuriTemplate}.
+     * @since 2.0.0
+     */
+    public String relPlaceHolderFrom(final String rel) {
+        if (isMatchingCuriedRel(rel)) {
+            return rel.substring(curiedRelPrefix.length());
+        }
+        if (isMatchingExpandedRel(rel)) {
+            return rel.substring(relPrefix.length(), rel.length() - relSuffix.length());
+        }
+        throw new IllegalArgumentException("Rel does not match the CURI template.");
+    }
+
+    /**
+     * Returns the {@code rel} in CURIED format.
+     *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     *
+     *     // Returns "x:product":
+     *     template.curiedRelFrom("http://example.org/rels/product");
+     *     template.curiedRelFrom("x:product"):
+     *
+     *     // Throws IllegalArgumentException:
+     *     template.curiedRelFrom("http://example.com/link-relations/product");
+     *     template.curiedRelFrom("y:product");
+     * </code></pre>
+     *
+     * @param rel a link-relation type.
+     * @return boolean
+     * @throws IllegalArgumentException if the {@code rel} parameter does not match the {@code CuriTemplate}.
+     * @since 2.0.0
+     */
+    public String curiedRelFrom(final String rel) {
+        if (isMatchingCuriedRel(rel)) {
+            return rel;
+        }
+        if (isMatchingExpandedRel(rel)) {
+            return curi.getName() + ":" + relPlaceHolderFrom(rel);
+        }
+        throw new IllegalArgumentException("Rel does not match the CURI template.");
+    }
+
+    /**
+     * Returns the {@code rel} in expanded format.
+     *
+     * <p>
+     *     Example:
+     * </p>
+     * <pre><code>
+     *     Link curi = Link.curi("x", "http://example.org/rels/{rel}");
+     *     CuriTemplate template = CuriTemplate.curiTemplateFor(curi);
+     *
+     *     // Returns "http://example.org/rels/product":
+     *     template.curiedRelFrom("http://example.org/rels/product");
+     *     template.curiedRelFrom("x:product"):
+     *
+     *     // Throws IllegalArgumentException:
+     *     template.curiedRelFrom("http://example.com/link-relations/product");
+     *     template.curiedRelFrom("y:product");
+     * </code></pre>
+     *
+     * @param rel a link-relation type.
+     * @return boolean
+     * @throws IllegalArgumentException if the {@code rel} parameter does not match the {@code CuriTemplate}.
+     * @since 2.0.0
+     */
+    public String expandedRelFrom(final String rel) {
+        if (isMatchingCuriedRel(rel)) {
+            return curi.getHrefAsTemplate().set("rel", relPlaceHolderFrom(rel)).expand();
+        }
+        if (isMatchingExpandedRel(rel)) {
+            return rel;
+        }
+        throw new IllegalArgumentException("Rel does not match the CURI template.");
+    }
+
+    /**
+     * Returns the curi {@link Link} of this template.
+     *
+     * @return curi
+     * @since 2.0.0
+     */
+    public Link getCuri() {
+        return curi;
     }
 }
--- src/test/java/de/otto/edison/hal/CuriTemplateTest.java
+++ src/test/java/de/otto/edison/hal/CuriTemplateTest.java
@@ -7,15 +7,30 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.List;
+import java.util.Optional;
+
+import static de.otto.edison.hal.CuriTemplate.curiTemplateFor;
+import static de.otto.edison.hal.CuriTemplate.matchingCuriTemplateFor;
 import static de.otto.edison.hal.Link.curi;
 import static de.otto.edison.hal.Link.link;
+import static de.otto.edison.hal.Link.linkBuilder;
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class CuriTemplateTest {
 
-    private final Link curi = curi("x", "http://example.org/rels/{rel}");
-    private final String rel = "http://example.org/rels/product";
+    private final Link someCuri = curi("x", "http://example.org/rels/{rel}");
+    private final Link someOtherCuri = curi("y", "http://example.com/link-relations/{rel}");
+
+    private final List<Link> curies = asList(someCuri, someOtherCuri);;
+
+    private final String someRel = "http://example.org/rels/product";
+    private final String someCuriedRel = "x:product";
+
+    private final String nonMatchingRel = "http://example.org/link-relations/product";
+    private final String nonMatchingCuriedRel = "z:product";
 
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
@@ -24,29 +39,96 @@ public class CuriTemplateTest {
     public void shouldFailToCreateCuriTemplateForWrongRel() {
         expectedEx.expect(IllegalArgumentException.class);
         expectedEx.expectMessage(matchesRegex(".*not a CURI link.*"));
-        CuriTemplate.curiTemplateFor(link("foo", "/bar"));
+        curiTemplateFor(link("foo", "/bar"));
     }
 
     @Test
     public void shouldFailToCreateCuriTemplateForWrongHref() {
         expectedEx.expect(IllegalArgumentException.class);
         expectedEx.expectMessage(matchesRegex(".*required.*placeholder.*"));
-        CuriTemplate.curiTemplateFor(link("curies", "/bar"));
+        curiTemplateFor(linkBuilder("curies","/bar").withName("x").build());
+    }
+
+    @Test
+    public void shouldFailToCreateCuriTemplateWithMissingName() {
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage("Parameter is not a CURI link.");
+        curiTemplateFor(link("curies", "/bar/{rel}"));
+    }
+
+    @Test
+    public void shouldFindMatchingUriTemplateForExpandedRel() {
+        final Optional<CuriTemplate> curiTemplate = matchingCuriTemplateFor(curies, someRel);
+        assertThat(curiTemplate.isPresent(), is(true));
+        assertThat(curiTemplate.get().getCuri(), is(someCuri));
+    }
+
+    @Test
+    public void shouldFindMatchingUriTemplateForCuriedRel() {
+        final Optional<CuriTemplate> curiTemplate = matchingCuriTemplateFor(curies, someCuriedRel);
+        assertThat(curiTemplate.isPresent(), is(true));
+        assertThat(curiTemplate.get().getCuri(), is(someCuri));
+    }
+
+    @Test
+    public void shouldNotFindMatchingUriTemplate() {
+        final Optional<CuriTemplate> curiTemplate = matchingCuriTemplateFor(curies, nonMatchingRel);
+        assertThat(curiTemplate.isPresent(), is(false));
     }
 
     @Test
     public void shouldMatch() {
-        assertThat(CuriTemplate.curiTemplateFor(curi).matches(rel), is(true));
+        assertThat(curiTemplateFor(someCuri).isMatching(someRel), is(true));
+        assertThat(curiTemplateFor(someCuri).isMatching(someCuriedRel), is(true));
+        assertThat(curiTemplateFor(someCuri).isMatchingExpandedRel(someRel), is(true));
+        assertThat(curiTemplateFor(someCuri).isMatchingCuriedRel(someCuriedRel), is(true));
+    }
+
+    @Test
+    public void shouldNotMatch() {
+        assertThat(curiTemplateFor(someCuri).isMatching(nonMatchingRel), is(false));
+        assertThat(curiTemplateFor(someCuri).isMatching(nonMatchingCuriedRel), is(false));
+        assertThat(curiTemplateFor(someCuri).isMatchingExpandedRel(someCuriedRel), is(false));
+        assertThat(curiTemplateFor(someCuri).isMatchingCuriedRel(someRel), is(false));
     }
 
     @Test
     public void shouldExtractCuriedRel() {
-        assertThat(CuriTemplate.curiTemplateFor(curi).curiedRelFrom(rel), is("x:product"));
+        assertThat(curiTemplateFor(someCuri).curiedRelFrom(someRel), is("x:product"));
+        assertThat(curiTemplateFor(someCuri).curiedRelFrom(someCuriedRel), is("x:product"));
+    }
+
+    @Test
+    public void shouldFailToExtractCuriedRelForNonMatchingRel() {
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage("Rel does not match the CURI template.");
+        curiTemplateFor(someCuri).curiedRelFrom(nonMatchingRel);
+    }
+
+    @Test
+    public void shouldExpandRel() {
+        assertThat(curiTemplateFor(someCuri).expandedRelFrom(someRel), is(someRel));
+        assertThat(curiTemplateFor(someCuri).expandedRelFrom(someCuriedRel), is(someRel));
+    }
+
+    @Test
+    public void shouldFailToExpandNonMatchingRel() {
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage("Rel does not match the CURI template.");
+        curiTemplateFor(someCuri).expandedRelFrom(nonMatchingRel);
     }
 
     @Test
     public void shouldExtractPlaceholderValue() {
-       assertThat(CuriTemplate.curiTemplateFor(curi).relPlaceHolderFrom(rel), is("product"));
+       assertThat(curiTemplateFor(someCuri).relPlaceHolderFrom(someRel), is("product"));
+       assertThat(curiTemplateFor(someCuri).relPlaceHolderFrom(someCuriedRel), is("product"));
+    }
+
+    @Test
+    public void shouldFailToExtractPlaceholderRelForNonMatchingRel() {
+        expectedEx.expect(IllegalArgumentException.class);
+        expectedEx.expectMessage("Rel does not match the CURI template.");
+        curiTemplateFor(someCuri).relPlaceHolderFrom(nonMatchingRel);
     }
 
     private Matcher<String> matchesRegex(final String regex) {
